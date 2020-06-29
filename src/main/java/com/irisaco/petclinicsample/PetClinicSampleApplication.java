6923f10e89ebd08ea9fc867dd70a19808289b7b7
fa-khademi <67546082+fa-khademi@users.noreply.github.com>
2020-06-29 17:20:05 +0430

    initial project

---
+++ src/main/java/com/irisaco/petclinicsample/PetClinicSampleApplication.java
@@ -0,0 +1,13 @@
+package com.irisaco.petclinicsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/irisaco/petclinicsample/PetClinicSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irisaco.petclinicsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
