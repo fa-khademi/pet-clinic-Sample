c7f9b03503bc8a6a712ed00451c8c78d421283a5
@fa.khademi.1362 <fa.khademi.1362@gmail.com>
2020-07-23 20:11:27 +0330

    added Entity JPA

--- pet-clinic-model/src/main/java/com/irisaco/petclinicsample/model/Vet.java
+++ pet-clinic-model/src/main/java/com/irisaco/petclinicsample/model/Vet.java
@@ -1,9 +1,15 @@
 package com.irisaco.petclinicsample.model;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_speciality" , joinColumns = @JoinColumn(name = "vet_id") ,
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities;
 
     public Set<Speciality> getSpecialities() {
